34e7e5167283d22e5fc19320f243c3d00db67cbc
Ricardo Oliveira <ricvenicius@gmail.com>
2024-09-07 10:35:47 -0300

    settings csrf

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', '0') == '1'
+DEBUG = os.getenv('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS: list[str] = ['*']
-
+ALLOWED_HOSTS: list[str] = [
+    'atomicdiscoveries.ricardovenicius.com.br', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://atomicdiscoveries.ricardovenicius.com.br'
+]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -80,12 +89,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': "basededados",
-        'USER': "usuario",
-        'PASSWORD': "senha",
-        'HOST': "127.0.0.1",
-        'PORT': "5432",
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
