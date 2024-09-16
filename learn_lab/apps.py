535ca2445f9333f252e15300baaae0e4538beb59
Ricardo Oliveira <ricvenicius@gmail.com>
2024-09-16 16:29:14 -0300

    learn_lab signals

--- learn_lab/apps.py
+++ learn_lab/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class LearnLabConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'learn_lab'
+
+    def ready(self, *args, **kwargs):
+        import learn_lab.signals  # noqa
+        super_ready = super().ready(*args, **kwargs)
+        return super_ready
---
+++ learn_lab/signals.py
@@ -0,0 +1,28 @@
+import os
+
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+
+from learn_lab.models import Activity
+
+
+def delete_file(instance):
+    try:
+        os.remove(instance.file.path)
+    except (ValueError, FileNotFoundError):
+        ...
+
+
+@receiver(pre_delete, sender=Activity)
+def delete_old_file(signal, instance, *args, **kwargs):
+    old_instance = Activity.objects.get(pk=instance.id)
+    delete_file(old_instance)
+
+
+@receiver(pre_save, sender=Activity)
+def update_activity_file(signal, instance, *args, **kwargs):
+    old_instance = Activity.objects.get(pk=instance.id)
+    is_new_instance = old_instance != instance.file
+
+    if is_new_instance:
+        delete_file(old_instance)
