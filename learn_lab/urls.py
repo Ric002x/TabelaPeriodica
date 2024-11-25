afd3581058ec61a7a1df94a04c0429825c226ab3
Ricardo Oliveira <ricvenicius@gmail.com>
2024-11-25 15:00:27 -0300

    Update all URLs to end with a trailing slash

--- learn_lab/urls.py
+++ learn_lab/urls.py
@@ -16,35 +16,35 @@ urlpatterns = [
     path('',
          views.LearnLabListView.as_view(), name='learn_lab_home'),
 
-    path('materia/<str:subject>',
+    path('materia/<str:subject>/',
          views.LearnLabSubjectListView.as_view(), name='learn_lab_subject'),
 
-    path('turma/<str:level>',
+    path('turma/<str:level>/',
          views.LearnLabLevelListView.as_view(), name='learn_lab_level'),
 
     path('atividade/buscar/',
          views.LearnLabListView.as_view(),
          name='learn_lab_activity_search'),
 
-    path('atividade/<slug:slug>',
+    path('atividade/<slug:slug>/',
          views.LearnLabDetailView.as_view(), name='learn_lab_activity'),
 
     path('atividade/criar/',
          views.activity_create, name='activity_create'),
 
-    path('atividade/editar/<slug:slug>',
+    path('atividade/editar/<slug:slug>/',
          views.activity_update, name='activity_update'),
 
-    path('atividade/deletar/<slug:slug>',
+    path('atividade/deletar/<slug:slug>/',
          views.activity_delete, name='activity_delete'),
 
-    path('atividade/<slug:slug>/avaliar', views.rating_create,
+    path('atividade/<slug:slug>/avaliar/', views.rating_create,
          name='activity_rate_create'),
 
-    path('atividade/<slug:slug>/editar', views.rating_edit,
+    path('atividade/<slug:slug>/editar/', views.rating_edit,
          name='activity_rate_edit'),
 
-    path('atividade/<slug:slug>/excluir', views.rating_delete,
+    path('atividade/<slug:slug>/excluir/', views.rating_delete,
          name='activity_rate_delete'),
 
     # Urls for APIs
--- users/urls.py
+++ users/urls.py
@@ -17,13 +17,13 @@ urlpatterns = [
     path(
         'cadastro/', views.register_view, name='register'),
     path(
-        'cadastro/criar', views.register_create, name='register_create'),
+        'cadastro/criar/', views.register_create, name='register_create'),
     path(
         'login/', views.login_view, name='login'),
     path(
-        'login/logar', views.login_create, name='login_create'),
+        'login/logar/', views.login_create, name='login_create'),
     path(
-        'perfil/update', views.perfil_update, name='profile_update'),
+        'perfil/update/', views.perfil_update, name='profile_update'),
     path(
         'perfil/<str:username>/',
         views.profile_user_data, name='profile_data'),
@@ -34,7 +34,7 @@ urlpatterns = [
 
     path('esqueci-minha-senha/', views.forgot_my_password,
          name="forgot_my_password"),
-    path('redefinir-senha/<str:uidb64>/<str:token>', views.reset_password,
+    path('redefinir-senha/<str:uidb64>/<str:token>/', views.reset_password,
          name="reset_password"),
 
     # SimpleJWT routs
